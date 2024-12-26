df2d3ea09268153376319ab57e246bf62de24265
EauDeData <evabot505@gmail.com>
2024-12-26 11:51:49 +0100

    zoomies seem to work

--- src/networks/encoder.py
+++ src/networks/encoder.py
@@ -75,7 +75,7 @@ class DisjointEncoder(nn.Module):
             nn.Linear(2048, FEATURE_DIM)
         )
 
-        self.local_extractor = resnet.resnet50(pretrained=True, pth_path=constants.PRETRAINED_EXTRACTOR_PATH) # resnet.LatentWordEncoder(final_dim=FEATURE_DIM)
+        # self.local_extractor = resnet.resnet50(pretrained=True, pth_path=constants.PRETRAINED_EXTRACTOR_PATH) # resnet.LatentWordEncoder(final_dim=FEATURE_DIM)
 
         self.query_extractor = TransformerSentenceEncoder(6, 4, FEATURE_DIM, vocab_size)
         self.DEVICE = device
@@ -83,8 +83,8 @@ class DisjointEncoder(nn.Module):
     def forward(self, global_view, local_views, query):
 
         batch, seq, _, c, h, w = local_views.shape
-        local_fm, local_embed_pre, _ = self.local_extractor(local_views.view(batch*seq, c, h, w)).view(batch, seq, c, h, w)
-        local_embeds = self.to_low_dim(local_embed_pre)
+        local_fm, local_embed_pre, _ = self.extractor(local_views.detach().view(batch*seq, c, h, w))
+        local_embeds = self.to_low_dim(local_embed_pre.view(batch, seq, -1))
 
         global_fm, global_embed_pre, _ = self.extractor(global_view.detach())
         global_embed = self.to_low_dim(global_embed_pre)
